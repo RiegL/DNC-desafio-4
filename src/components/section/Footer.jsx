5b521af126e6ca44f33caec590e3d994d1b1a69b
RiegL <leoorieg@gmail.com>
2024-05-09 22:09:27 -0300

    adicionado no footer a hora em tempo real, usando useState e useEffect e a biblioteca dayjs

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,14 +1,31 @@
 import sx from './Footer.module.css';
 import { FaFigma, FaLinkedin, FaGithub } from "react-icons/fa";
-// import dayjs from 'dayjs';
+import dayjs from 'dayjs';
+import { useState } from 'react';
+import { useEffect } from 'react';
 // import { useState, useEffect } from 'react';
 
 function Footer(){
+
+    const [ hour, setHour] = useState()
+    
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+           setHour(dayjs().format('HH:mm:ss'));
+        },1000);
+        return()=>{
+            clearInterval(interval)
+        }
+    },[hour])
+
+
+
     return(
         <div className={sx.footer}>
             <div className={sx.contacts}>
             <p>Meu contato:<br/>47 99600-5026</p>
             <p>Email:<br/>leoorieg@gmail.com</p> 
+            <p>{hour}</p>
             </div>
 
         <div>
